37b7c8271605baff69064fe8016039fdc80ca862
EBNLL <erickbenelli08@gmail.com>
2026-04-16 22:13:01 -0300

    js

---
+++ 01-JS/testes.js
@@ -0,0 +1,21 @@
+//alert("Olá mundo!")
+//alert("Segunda mensagem")
+
+//let nome 
+
+//nome = "Erick"
+
+//alert("Nome: " + nome)
+//let idade = 30
+//alert("idade: " + idade)
+
+//alert("Nome: " + nome + "\nIdade: " + idade)
+
+//nome = prompt("Digite seu nome: ")
+//alert("Nome: " + nome)
+let nota1, nota2
+
+nota1 = prompt("Qual foi a sua nota? " )
+nota1 = Number(nota1)
+nota1 = nota1 
+alert("Nota: " + nota1)
