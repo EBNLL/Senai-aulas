5f3b37c44ce8c59d8e0bee7e34e50682e5de26a7
EBNLL <erickbenelli08@gmail.com>
2026-05-21 22:06:22 -0300

    Atividades

--- variaveis/script.js
+++ variaveis/script.js
@@ -1,200 +1,267 @@
-let numeroSorteio, valorAposta
+let valorTotal, valorDoRepasse, valorFaturado, clientes = 0, descontos
 
-function apostar(){
+function totalRecebido(){
+        
+        clientes++
+        valorTotal = Number(document.getElementById("totalv").value)
+        valorDoRepasse = valorTotal * 0.10
+        valorFaturado = valorTotal - valorDoRepasse
+        document.getElementById("valort").innerHTML = "Valor do recibo: R$" + valorTotal
+        document.getElementById("repassec").innerHTML = "Valor do repasse para o Seu Celso: R$" + valorDoRepasse
+        document.getElementById("desconto").innerHTML = "Valor faturado: R$" + valorFaturado
+        document.getElementById("clientela").innerHTML = "Clientes: " + clientes
+        
+        if(clientes == 10){
+            descontos = valorTotal * 0.05
+            valorTotal = valorTotal - descontos
+            document.getElementById("desconte").innerHTML = "Valor com 5% de desconto: R$" + valorTotal
+        }
+        else if(clientes == 11){
+            clientes = 1
+            document.getElementById("desconte").innerHTML = " "
+            
+        }
+            
+    }
     
-    valorAposta = Number(document.getElementById("barraAposta").value)
-    numeroSorteio = Number(Math.ceil(Math.random()*10))
-    console.log(numeroSorteio)
 
-}
+  
 
-function espinossauro(){
 
-        document.getElementById("dino").innerHTML = "Dinossauro escolhido: Espinossauro"
-        if(numeroSorteio == 1){
-            valorAposta = valorAposta * 2
-            document.getElementById("dinheiro").innerHTML = "R$ " + valorAposta
-            dinoSorteado()
-        }
-        else{
-            valorAposta = 0
-            document.getElementById("dinheiro").innerHTML = "R$ " + valorAposta
-            dinoSorteado()
-        } 
 
-    }
-    
-    function tRex(){
-        
-        document.getElementById("dino").innerHTML = "Dinossauro escolhido: T-rex"
-        if(numeroSorteio == 2){
-            valorAposta = valorAposta * 2
-            document.getElementById("dinheiro").innerHTML = "R$ " + valorAposta
-            dinoSorteado()
-        }
-        else{
-            valorAposta = 0
-            document.getElementById("dinheiro").innerHTML = "R$ " + valorAposta
-            dinoSorteado()
-        }   
-    }
-    
-    function branquiossauro(){
-        document.getElementById("dino").innerHTML = "Dinossauro escolhido: Branquiossauro"
-        if(numeroSorteio == 3){
-            valorAposta = valorAposta * 2
-            document.getElementById("dinheiro").innerHTML = "R$ " + valor
-            dinoSorteado()
-        }
-        else{
-            valorAposta = 0
-            document.getElementById("dinheiro").innerHTML = "R$ " + valorAposta
-            dinoSorteado()
-        }   
-    }
-    
-    function velocirraptor(){
-        document.getElementById("dino").innerHTML = "Dinossauro escolhido: Velocirraptor"
-        if(numeroSorteio == 4){
-            valorAposta = valorAposta * 2
-            document.getElementById("dinheiro").innerHTML = "R$ " + valorAposta
-            dinoSorteado()
-        }
-        else{
-            valorAposta = 0
-            document.getElementById("dinheiro").innerHTML = "R$ " + valorAposta
-            dinoSorteado()
-        }   
-    }
-    
-    function estegossauro(){
-        document.getElementById("dino").innerHTML = "Dinossauro escolhido: Estegossauro"
-        if(numeroSorteio == 5){
-            valorAposta = valorAposta * 2
-            document.getElementById("dinheiro").innerHTML = "R$ " + valorAposta
-            dinoSorteado()
-        }
-        else{
-            valorAposta = 0
-            document.getElementById("dinheiro").innerHTML = "R$ " + valorAposta
-            dinoSorteado()
-        }   
-    }
-    
-    function triceratopo(){
-        document.getElementById("dino").innerHTML = "Dinossauro escolhido: Triceratopo"
-        if(numeroSorteio == 6){
-            valorAposta = valorAposta * 2
-            document.getElementById("dinheiro").innerHTML = "R$ " + valorAposta
-            dinoSorteado()
-        }
-        else{
-            valorAposta = 0
-            document.getElementById("dinheiro").innerHTML = "R$ " + valorAposta
-            dinoSorteado()
-        }   
-    }
-    
-    function anquilossauro(){
-        document.getElementById("dino").innerHTML = "Dinossauro escolhido: Anquilossauro"
-        if(numeroSorteio == 7){
-            valorAposta = valorAposta * 2
-            document.getElementById("dinheiro").innerHTML = "R$ " + valorAposta
-            dinoSorteado()
-        }
-        else{
-            valorAposta = 0
-            document.getElementById("dinheiro").innerHTML = "R$ " + valorAposta
-            dinoSorteado()
-        }   
-    }
-    
-    function parassauroloio(){
-        document.getElementById("dino").innerHTML = "Dinossauro escolhido: Parassauroloio"
-        if(numeroSorteio == 8){
-            valorAposta = valorAposta * 2
-            document.getElementById("dinheiro").innerHTML = "R$ " + valorAposta
-            dinoSorteado()
-        }
-        else{
-            valorAposta = 0
-            document.getElementById("dinheiro").innerHTML = "R$ " + valorAposta
-            dinoSorteado()
-        }     
-    }
-    
-    function alessauro(){
-        document.getElementById("dino").innerHTML = "Dinossauro escolhido: Alessauro"
-        if(numeroSorteio == 9){
-            valorAposta = valorAposta * 2
-            document.getElementById("dinheiro").innerHTML = "R$ " + valorAposta
-            dinoSorteado()
-        }
-        else{
-            valorAposta = 0
-            document.getElementById("dinheiro").innerHTML = "R$ " + valorAposta
-            dinoSorteado()
-        }   
-        
-    }
-    
-    function carnotouro(){
-        document.getElementById("dino").innerHTML = "Dinossauro escolhido: Carnotouro"
-        if(numeroSorteio == 10){
-            valorAposta = valorAposta * 2
-       document.getElementById("dinheiro").innerHTML = "R$ " + valorAposta
-       dinoSorteado()
-    }
-    else{
-        valorAposta = 0
-        document.getElementById("dinheiro").innerHTML = "R$ " + valorAposta
-        dinoSorteado()
-    }   
-}
 
-    function dinoSorteado(){
-    if(numeroSorteio == 1){
-        
-        document.getElementById("dino_sorte").innerHTML = "Dinossauro sorteado: Espinossauro"
-    }
-    if(numeroSorteio == 2){
-        
-        document.getElementById("dino_sorte").innerHTML = "Dinossauro sorteado: T-rex"
-    }
-    if(numeroSorteio == 3){
-        
-        document.getElementById("dino_sorte").innerHTML = "Dinossauro sorteado: Branquiossauro"
-    }
-    if(numeroSorteio == 4){
-        
-        document.getElementById("dino_sorte").innerHTML = "Dinossauro sorteado: Velocirraptor"
-    }
-    if(numeroSorteio == 5){
-        
-        document.getElementById("dino_sorte").innerHTML = "Dinossauro sorteado: Estegossauro"
-    }
-    if(numeroSorteio == 6){
-        
-        document.getElementById("dino_sorte").innerHTML = "Dinossauro sorteado: Triceratopo"
-    }
-    if(numeroSorteio == 7){
-        
-        document.getElementById("dino_sorte").innerHTML = "Dinossauro sorteado: Anquilossauro"
-    }
-    if(numeroSorteio == 8){
-        
-        document.getElementById("dino_sorte").innerHTML = "Dinossauro sorteado: Parassauro"
-    }
-    if(numeroSorteio == 9){
-        
-        document.getElementById("dino_sorte").innerHTML = "Dinossauro sorteado: Alossauro"
-    }
-    if(numeroSorteio == 10){
-        
-        document.getElementById("dino_sorte").innerHTML = "Dinossauro sorteado: Carnotouro"
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let numeroSorteio, valorAposta
+
+// function apostar(){
     
-    }
+//     valorAposta = Number(document.getElementById("barraAposta").value)
+//     numeroSorteio = Number(Math.ceil(Math.random()*10))
+//     console.log(numeroSorteio)
+
+// }
+
+// function espinossauro(){
+
+//         document.getElementById("dino").innerHTML = "Dinossauro escolhido: Espinossauro"
+//         if(numeroSorteio == 1){
+//             valorAposta = valorAposta * 2
+//             document.getElementById("dinheiro").innerHTML = "R$ " + valorAposta
+//             dinoSorteado()
+//         }
+//         else{
+//             valorAposta = 0
+//             document.getElementById("dinheiro").innerHTML = "R$ " + valorAposta
+//             dinoSorteado()
+//         } 
+
+//     }
+    
+//     function tRex(){
+        
+//         document.getElementById("dino").innerHTML = "Dinossauro escolhido: T-rex"
+//         if(numeroSorteio == 2){
+//             valorAposta = valorAposta * 2
+//             document.getElementById("dinheiro").innerHTML = "R$ " + valorAposta
+//             dinoSorteado()
+//         }
+//         else{
+//             valorAposta = 0
+//             document.getElementById("dinheiro").innerHTML = "R$ " + valorAposta
+//             dinoSorteado()
+//         }   
+//     }
+    
+//     function branquiossauro(){
+//         document.getElementById("dino").innerHTML = "Dinossauro escolhido: Branquiossauro"
+//         if(numeroSorteio == 3){
+//             valorAposta = valorAposta * 2
+//             document.getElementById("dinheiro").innerHTML = "R$ " + valor
+//             dinoSorteado()
+//         }
+//         else{
+//             valorAposta = 0
+//             document.getElementById("dinheiro").innerHTML = "R$ " + valorAposta
+//             dinoSorteado()
+//         }   
+//     }
+    
+//     function velocirraptor(){
+//         document.getElementById("dino").innerHTML = "Dinossauro escolhido: Velocirraptor"
+//         if(numeroSorteio == 4){
+//             valorAposta = valorAposta * 2
+//             document.getElementById("dinheiro").innerHTML = "R$ " + valorAposta
+//             dinoSorteado()
+//         }
+//         else{
+//             valorAposta = 0
+//             document.getElementById("dinheiro").innerHTML = "R$ " + valorAposta
+//             dinoSorteado()
+//         }   
+//     }
+    
+//     function estegossauro(){
+//         document.getElementById("dino").innerHTML = "Dinossauro escolhido: Estegossauro"
+//         if(numeroSorteio == 5){
+//             valorAposta = valorAposta * 2
+//             document.getElementById("dinheiro").innerHTML = "R$ " + valorAposta
+//             dinoSorteado()
+//         }
+//         else{
+//             valorAposta = 0
+//             document.getElementById("dinheiro").innerHTML = "R$ " + valorAposta
+//             dinoSorteado()
+//         }   
+//     }
+    
+//     function triceratopo(){
+//         document.getElementById("dino").innerHTML = "Dinossauro escolhido: Triceratopo"
+//         if(numeroSorteio == 6){
+//             valorAposta = valorAposta * 2
+//             document.getElementById("dinheiro").innerHTML = "R$ " + valorAposta
+//             dinoSorteado()
+//         }
+//         else{
+//             valorAposta = 0
+//             document.getElementById("dinheiro").innerHTML = "R$ " + valorAposta
+//             dinoSorteado()
+//         }   
+//     }
+    
+//     function anquilossauro(){
+//         document.getElementById("dino").innerHTML = "Dinossauro escolhido: Anquilossauro"
+//         if(numeroSorteio == 7){
+//             valorAposta = valorAposta * 2
+//             document.getElementById("dinheiro").innerHTML = "R$ " + valorAposta
+//             dinoSorteado()
+//         }
+//         else{
+//             valorAposta = 0
+//             document.getElementById("dinheiro").innerHTML = "R$ " + valorAposta
+//             dinoSorteado()
+//         }   
+//     }
+    
+//     function parassauroloio(){
+//         document.getElementById("dino").innerHTML = "Dinossauro escolhido: Parassauroloio"
+//         if(numeroSorteio == 8){
+//             valorAposta = valorAposta * 2
+//             document.getElementById("dinheiro").innerHTML = "R$ " + valorAposta
+//             dinoSorteado()
+//         }
+//         else{
+//             valorAposta = 0
+//             document.getElementById("dinheiro").innerHTML = "R$ " + valorAposta
+//             dinoSorteado()
+//         }     
+//     }
+    
+//     function alessauro(){
+//         document.getElementById("dino").innerHTML = "Dinossauro escolhido: Alessauro"
+//         if(numeroSorteio == 9){
+//             valorAposta = valorAposta * 2
+//             document.getElementById("dinheiro").innerHTML = "R$ " + valorAposta
+//             dinoSorteado()
+//         }
+//         else{
+//             valorAposta = 0
+//             document.getElementById("dinheiro").innerHTML = "R$ " + valorAposta
+//             dinoSorteado()
+//         }   
+        
+//     }
+    
+//     function carnotouro(){
+//         document.getElementById("dino").innerHTML = "Dinossauro escolhido: Carnotouro"
+//         if(numeroSorteio == 10){
+//             valorAposta = valorAposta * 2
+//        document.getElementById("dinheiro").innerHTML = "R$ " + valorAposta
+//        dinoSorteado()
+//     }
+//     else{
+//         valorAposta = 0
+//         document.getElementById("dinheiro").innerHTML = "R$ " + valorAposta
+//         dinoSorteado()
+//     }   
+// }
+
+//     function dinoSorteado(){
+//     if(numeroSorteio == 1){
+        
+//         document.getElementById("dino_sorte").innerHTML = "Dinossauro sorteado: Espinossauro"
+//     }
+//     if(numeroSorteio == 2){
+        
+//         document.getElementById("dino_sorte").innerHTML = "Dinossauro sorteado: T-rex"
+//     }
+//     if(numeroSorteio == 3){
+        
+//         document.getElementById("dino_sorte").innerHTML = "Dinossauro sorteado: Branquiossauro"
+//     }
+//     if(numeroSorteio == 4){
+        
+//         document.getElementById("dino_sorte").innerHTML = "Dinossauro sorteado: Velocirraptor"
+//     }
+//     if(numeroSorteio == 5){
+        
+//         document.getElementById("dino_sorte").innerHTML = "Dinossauro sorteado: Estegossauro"
+//     }
+//     if(numeroSorteio == 6){
+        
+//         document.getElementById("dino_sorte").innerHTML = "Dinossauro sorteado: Triceratopo"
+//     }
+//     if(numeroSorteio == 7){
+        
+//         document.getElementById("dino_sorte").innerHTML = "Dinossauro sorteado: Anquilossauro"
+//     }
+//     if(numeroSorteio == 8){
+        
+//         document.getElementById("dino_sorte").innerHTML = "Dinossauro sorteado: Parassauro"
+//     }
+//     if(numeroSorteio == 9){
+        
+//         document.getElementById("dino_sorte").innerHTML = "Dinossauro sorteado: Alossauro"
+//     }
+//     if(numeroSorteio == 10){
+        
+//         document.getElementById("dino_sorte").innerHTML = "Dinossauro sorteado: Carnotouro"
+//     }
+    
+//     }
 
 
 
