fb4ea74df6393aad9a6f224011442ff2d5460f46
EBNLL <erickbenelli08@gmail.com>
2026-07-17 21:59:51 -0300

    Atividade

--- crud/script.js
+++ crud/script.js
@@ -2,7 +2,7 @@
 
 const dinos = []
 
-function testar() {
+function adicionardinos() {
     let novoDino = {
         id: Date.now(),
         nome: document.getElementById("input-nome").value,
@@ -14,22 +14,77 @@ function testar() {
     dinos.push(novoDino)
     console.log(dinos)
     limparFormulario()
+    mostrardinos()
 }
-function limparFormulario(){
+function limparFormulario() {
     document.getElementById("input-nome").value = ""
     document.getElementById("input-altura").value = ""
     document.getElementById("input-cor").value = ""
     document.getElementById("input-custo").value = ""
-    
+    document.getElementById("input-id").value = ""
+
     document.getElementById("input-nome").focus()
 }
-function mostrardinos(){
-    dinos
-    for(i=0;i<dinos.length;i++){
-        document.getElementById("m-d").innerHTML += dinos[i].nome + ", "
-        document.getElementById("m-d").innerHTML += dinos[i].altura + ", "
-        document.getElementById("m-d").innerHTML += dinos[i].cor + ","
-        document.getElementById("m-d").innerHTML += dinos[i].custo + " <br>"
-   
+function mostrardinos() {
+    document.getElementById("m-d").innerHTML = ""
+
+    for (i = 0; i < dinos.length; i++) {
+
+        document.getElementById("m-d").innerHTML += "id: " + dinos[i].id + "<br>"
+        document.getElementById("m-d").innerHTML += "Nome: " + dinos[i].nome + "<br>"
+        document.getElementById("m-d").innerHTML += "Altura: " + dinos[i].altura + "<br>"
+        document.getElementById("m-d").innerHTML += "Cor" + dinos[i].cor + "<br>"
+        document.getElementById("m-d").innerHTML += "Custo" + dinos[i].custo + "<br>"
+
     }
+}
+function removerdino() {
+    let remove = document.getElementById("input-nome").value
+    for (i = 0; i < dinos.length; i++) {
+        if (remove == dinos[i].nome) {
+            dinos.splice(i, 1)
+        }
+    }
+    limparFormulario()
+    mostrardinos()
+}
+
+function altdino() {
+    let troca = document.getElementById("input-id").value
+    let alterar = document.getElementById("input-nome").value
+    let alterar2 = Number(document.getElementById("input-altura").value)
+    let alterar3 = document.getElementById("input-cor").value
+    let alterar4 = Number(document.getElementById("input-custo").value)
+    for (i = 0; i < dinos.length; i++) {
+        if (troca == dinos[i].id) {
+            if (alterar != "") {
+                dinos[i].nome = alterar
+            }
+            if (alterar2 != "") {
+                dinos[i].altura = alterar2
+            }
+            if (alterar3 != "") {
+                dinos[i].cor = alterar3
+            }
+            if (alterar4 != "")
+                dinos[i].custo = alterar4
+        }
+          
+    }
+    limparFormulario()
+    mostrardinos()
+}
+function procurarDino(){
+    
+    let procuraDino = document.getElementById("input-nome").value
+   
+    for (i = 0; i < dinos.length; i++) {
+        if (procuraDino == dinos[i].nome) {
+        document.getElementById("input-nome").value = dinos.nome
+        document.getElementById("input-altura").value = dinos.altura
+        document.getElementById("input-cor").value = dinos.cor
+        document.getElementById("input-custo").value = dinos.custo
+    }
+    mostrardinos()
+}
 }
